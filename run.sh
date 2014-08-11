7f027c695a91317008ce77eb7816d8d3f873aa79
Pascal Armand <pascalito@gmail.com>
2014-08-11 11:32:19 +0200

    add param for timestap - put captures into "dir per day"

--- run.sh
+++ run.sh
@@ -24,30 +24,31 @@ XVFB_DISPLAY=23
 
 #Setting for timestamp
 TIMESTAMP=false
+TIMESTAMP_SIZE=12
+TIMESTAMP_FORMAT=`date +"%d-%m-%Y %H:%M:%S"`
 
 while getopts ":t" opt; do
-	  case $opt in
-		t)
-		  TIMESTAMP=true
-		  ;;
-		\?)
-		  echo "Invalid option: -$OPTARG" >&2
-		  ;;
-	  esac
-	done
-	
+    case $opt in
+        t)
+            TIMESTAMP=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
 echo "Staring XVFB on $XVFB_DISPLAY"
 Xvfb :${XVFB_DISPLAY} -screen 0 ${XVFB_RES_WIDTH}x${XVFB_RES_HEIGHT}x24 -noreset -nolisten tcp 2> /dev/null &
 XVFB_PID=$!
 
 add_timestamp () {
-	img_name=$1
-	XTEXT=`expr $SCREENSHOT_WIDTH - 50`
-	YTEXT=`expr $SCREENSHOT_HEIGHT + 175`
-	STRINGTEXT="text $XTEXT,$YTEXT '"
-	PRETTY_DATE=`date +"%d-%m-%Y %H:%M:%S"`
-	STRINGTEXT="$STRINGTEXT$PRETTY_DATE'"
-	convert -pointsize 20 -fill yellow -draw "$STRINGTEXT" $img_name $img_name
+    img_name=$1
+    XTEXT=`expr $SCREENSHOT_WIDTH - 50`
+    YTEXT=`expr $SCREENSHOT_HEIGHT + 175`
+    STRINGTEXT="text $XTEXT,$YTEXT '"
+    STRINGTEXT="$STRINGTEXT$TIMESTAMP_FORMAT'"
+    convert -pointsize $TIMESTAMP_SIZE -fill yellow -draw "$STRINGTEXT" $img_name $img_name
 }
 
 while true
@@ -55,9 +56,9 @@ do
     # Remove parent lock to prevent error message "firefox has been shutdown unexpectly..."
     rm ~/.mozilla/firefox/${FF_PROFILE_DIR}/.parentlock
 
-	# Flush cache to prevent "Reload page" message (this happens anyway, but not that often)
-	echo "Flushing cache "
-	rm -r ~/.cache/mozilla/firefox/${FF_PROFILE_DIR}/*
+    # Flush cache to prevent "Reload page" message (this happens anyway, but not that often)
+    echo "Flushing cache "
+    rm -r ~/.cache/mozilla/firefox/${FF_PROFILE_DIR}/*
 
     echo "Running firefox -P $FF_PROFILE on $XVFB_DISPLAY "
     DISPLAY=:${XVFB_DISPLAY} firefox -P $FF_PROFILE -width $XVFB_RES_WIDTH -height $XVFB_RES_HEIGHT "$FF_URL" > /dev/null &
@@ -69,13 +70,18 @@ do
     sleep $FF_WAIT;
 
     echo "Taking screenshot. Please smile!"
-    HAM_DATE=`date +"%Y-%m-%d_%H-%M-%S"`
-    DISPLAY=:${XVFB_DISPLAY} import -window root -crop ${SCREENSHOT_WIDTH}x${SCREENSHOT_HEIGHT}+${SCREENSHOT_OFFSET_LEFT}+${SCREENSHOT_OFFSET_TOP} "ingr-$HAM_DATE".png
-   if $TIMESTAMP; then
-		add_timestamp ingr-$HAM_DATE.png
-	fi
-	   
-	
+    HAM_DATE1=`date +"%Y-%m-%d"`
+    HAM_DATE2=`date +"%H-%M-%S"`
+    if [ ! -d "$HAM_DATE1" ]; then
+        mkdir $HAM_DATE1
+    fi
+    HAM_DATE=$HAM_DATE1/$HAM_DATE2
+    DISPLAY=:${XVFB_DISPLAY} import -window root -crop ${SCREENSHOT_WIDTH}x${SCREENSHOT_HEIGHT}+${SCREENSHOT_OFFSET_LEFT}+${SCREENSHOT_OFFSET_TOP} "$HAM_DATE1/$FF_PROFILE-$HAM_DATE2.png"
+    if $TIMESTAMP; then
+        add_timestamp $HAM_DATE1/$FF_PROFILE-$HAM_DATE2.png
+    fi
+
+
     echo "Killing firefox on PID $FF_PID"
     kill $FF_PID
 
